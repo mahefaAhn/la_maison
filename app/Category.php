fbdd1ac990ae64bbed876c17a46f57b7d3a5328b
Mahefa <mahefa.ahn@gmail.com>
2020-06-02 16:05:05 +0200

    Correction de l'erreur
    Renommer Categories par Category

--- app/Category.php
+++ app/Category.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Categories extends Model
+class Category extends Model
 {
     // Une catégorie peut avoir un ou plusieurs produits
     public function products(){
