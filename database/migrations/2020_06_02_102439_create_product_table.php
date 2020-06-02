8e726aaa83c1c7270feb782c74b1d7f6b501bfec
Mahefa <mahefa.ahn@gmail.com>
2020-06-02 13:10:05 +0200

    Initialisation du projet

---
+++ database/migrations/2020_06_02_102439_create_product_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 100);
+            $table->text('description')->nullable();
+            $table->decimal('price',8,2);
+            $table->enum('size', ['46','48','50','52'])->default('46');
+            $table->text('url')->nullable();
+            $table->enum('status', ['published', 'unpublished'])->default('unpublished');
+            $table->enum('code', ['solde', 'new'])->default('new');
+            $table->string('reference', 100);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product');
+    }
+}
